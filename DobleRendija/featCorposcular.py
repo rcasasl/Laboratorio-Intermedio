8d40d92e96388ca89bbf5eb51babcb6b297c36db
jfCamelo <j.camelob@uniandes.edu.co>
2024-04-20 21:14:53 -0500

    cambios corp

--- DobleRendija/featCorposcular.py
+++ DobleRendija/featCorposcular.py
@@ -6,8 +6,8 @@ import pandas as pd
 laserDoble = "DobleRendija\Laser1rendjia.xlsx"
 laser2df = pd.read_excel(laserDoble)
 print(laser2df)
-laser2X = laser2df.iloc[int(0.1 * len(laser2df)):int(0.9 * len(laser2df)), 0].values
-laser2V = laser2df.iloc[int(0.1 * len(laser2df)):int(0.9 * len(laser2df)), 1].values
+laser2X = laser2df.iloc[int(0 * len(laser2df)):int(1 * len(laser2df)), 0].values
+laser2V = laser2df.iloc[int(0 * len(laser2df)):int(1 * len(laser2df)), 1].values
 
 # Define the equation
 def intensity(params, x):
@@ -20,7 +20,7 @@ def loss(params, x, observed):
     return np.mean((predicted - observed) ** 2)
 
 # Initial guess for parameters
-initial_guess = [0.01, 0.01, 0.01, 1e-6]  # Adjust as needed
+initial_guess = [1, 0.0005, 0.005, 0.05]  # Adjust as needed
 
 # Minimize the loss function to find the best parameters
 result = minimize(loss, initial_guess, args=(laser2X, laser2V),method='Nelder-Mead')
