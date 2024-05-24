ce9ca461ef3ae26eaf5657a84b1b70d8de9f6095
Rafael Casas <rafaelcasaslozano@gmail.com>
2024-05-24 01:16:23 -0400

    Difraccion de electrones final

---
+++ dif_electrones/linregress.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class linregress_w:
+    def __init__(self, x, y, dy):
+        self.x= x
+        self.y = y
+        self.dy = dy
+        self.w = 1/dy**2
+        self.D= np.sum(self.w*np.sum(self.w*(self.x**2))) - (np.sum(self.w*self.x))**2
+    def slope(self):
+        return (np.sum(self.w*np.sum(self.w*self.x*self.y))  -  np.sum(self.w*self.x*np.sum(self.w*self.y)))/self.D
+    def err_slope(self):
+        return np.sqrt(np.sum(self.w)/self.D)
+    def intercept(self):
+        return (np.sum(self.w*(self.x)**2*np.sum(self.w*self.y)) - np.sum(self.w*self.x*np.sum(self.w*self.x*self.y)))/self.D
+    def err_intercept(self):
+        return np.sqrt(np.sum(self.w*self.x**2)/self.D)
